fffffec331f2f4cc239984f18a3b0eaac338af96
Vaibhav Singh Bains <131239955+vaibhavvst24@users.noreply.github.com>
2026-05-21 12:50:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -176,7 +176,7 @@ footer {visibility: hidden;}
 """, unsafe_allow_html=True)
 
 st.markdown('<div class="title">Review Sentiment Analysis System</div>', unsafe_allow_html=True)
-st.markdown('<div class="subtitle">NLP-based sentiment analysis system</div>', unsafe_allow_html=True)
+st.markdown('<div class="subtitle">NLP-based sentiment analytics system</div>', unsafe_allow_html=True)
 
 user_text = st.text_area(
     "Enter customer support message...",
